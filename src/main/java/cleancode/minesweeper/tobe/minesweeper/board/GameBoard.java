7e9ab68fdb61076ab16044eb141c206e6d99307c
lkwoung88 <lkwoung88@gmail.com>
2025-03-10 23:03:17 +0900

    # 30. IDE 사용하기 - cmd+opt+L - sonarlint - editorconfig

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/GameBoard.java
@@ -6,6 +6,8 @@ import cleancode.minesweeper.tobe.minesweeper.board.position.CellPosition;
 import cleancode.minesweeper.tobe.minesweeper.board.position.CellPositions;
 import cleancode.minesweeper.tobe.minesweeper.board.position.RelativePosition;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.Stack;
 
@@ -148,17 +150,18 @@ public class GameBoard {
     }
 
     private void openSurroundedCells(CellPosition cellPosition) {
-        Stack<CellPosition> stack = new Stack<>();
-        stack.push(cellPosition);
+        Deque<CellPosition> deque = new ArrayDeque<>();
 
-        while (!stack.isEmpty()) {
-            openAndPushCellAt(stack);
+        deque.push(cellPosition);
+
+        while (!deque.isEmpty()) {
+            openAndPushCellAt(deque);
         }
     }
 
-    private void openAndPushCellAt(Stack<CellPosition> stack) {
+    private void openAndPushCellAt(Deque<CellPosition> deque) {
 
-        CellPosition currentCellPosition = stack.pop();
+        CellPosition currentCellPosition = deque.pop();
 
         if (currentCellPosition.isRowIndexMoreThanOrEqualTo(getRowSize())
                 || currentCellPosition.isColumnIndexMoreThanOrEqualTo(getColSize())) {
@@ -181,7 +184,7 @@ public class GameBoard {
 
         List<CellPosition> surroundedPositions = calculateSurroundedPositions(currentCellPosition, getRowSize(), getColSize());
         for (CellPosition position : surroundedPositions) {
-            stack.push(position);
+            deque.push(position);
         }
     }
 
