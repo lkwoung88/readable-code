7d65fa304c8b8b4c7604d611b9d8e8d800e1035a
lkwoung88 <lkwoung88@gmail.com>
2025-03-15 01:56:56 +0900

    # 36. 리팩토링(3) - FileHandler 를 Provider 개념으로 리팩토링

---
+++ src/main/java/cleancode/studycafe/tobe/provider/SeatPassProvider.java
@@ -0,0 +1,10 @@
+package cleancode.studycafe.tobe.provider;
+
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPasses;
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPass;
+
+public interface SeatPassProvider {
+
+    StudyCafeSeatPasses getSeatPasses();
+}
+
